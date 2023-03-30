c6b615f39b1fc39ef8fec02f2e02af2edc9844a0
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2023-03-29 20:41:55 -0700

    working on hello view and java

--- src/main/java/com/example/inventory_program/HelloApplication.java
+++ src/main/java/com/example/inventory_program/HelloApplication.java
@@ -20,7 +20,7 @@ public class HelloApplication extends Application {
     //public void start(Stage stage) throws Exception {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
 //        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
 //        Scene scene = new Scene(root);
         stage.setTitle("Inventory Management System");
