61ddc7c30f46988e7176afa8c2bf43d38f686a36
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2023-05-03 11:56:23 -0700

    adding java doc commands to PartsAndProductsInventory.java

--- src/main/java/com/example/inventory_program/PartsAndProductsInventory.java
+++ src/main/java/com/example/inventory_program/PartsAndProductsInventory.java
@@ -2,10 +2,19 @@ package com.example.inventory_program;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+/**
+ * @author Evelyn G Morrow.
+ * @version 1.0.
+ * Public class PartsAndProductsInventory is used to manage the products and parts that EM Management System has in inventory.
+ */
 public class PartsAndProductsInventory {
     private ObservableList<ProductData> allProducts = FXCollections.observableArrayList();
     private ObservableList<PartData> allParts = FXCollections.observableArrayList();
 
+    /**
+     * Public void updatePart() method takes the updatePartData parameter from the PartDara public class.
+     * @param updatePartData is used to retrieve the partID od the part we are trying to update.
+     */
     public void updatePart(PartData updatePartData) {
         for(int i = 0; i < allParts.size(); i++) {
             if(allParts.get(i).getPartID() == updatePartData.getPartID()) {
@@ -16,6 +25,10 @@ public class PartsAndProductsInventory {
         return;
     }
 
+    /**
+     * Public ObeservableList gets all the parts from the PartDara public class.
+     * @return allParts.
+     */
     public ObservableList<PartData> getAllParts() {
         return allParts;
     }
